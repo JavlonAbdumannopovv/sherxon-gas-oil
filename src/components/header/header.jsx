eedc38f8d74840cc6aed5295f53ebcc0ddac6788
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-07-15 14:03:14 +0500

    netlify deploy finish

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="bg-[#171923] w[100%] h-[70px] flex justify-center items-center mx-4 mb-4 rounded-lg">
-      <h1 className='text-white text-lg'>Gasolin Bugaltery</h1>
+      <h1 className='text-white text-lg'>Sherxon Gas-Oil</h1>
     </div>
   )
 }
